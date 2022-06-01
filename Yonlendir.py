7b2aa43704e9749b62517c01b28756678d4f64b8
Ayşenur DENİZ <anurdenizz@gmail.com>
2022-06-01 13:29:57 +0300

    Yonlendir.py edited

--- Yonlendir.py
+++ Yonlendir.py
@@ -15,6 +15,7 @@ timerr = Timer()
 @app.route("/", methods=["GET", "POST"])
 def index():
     my_title = "Full Text Search"
+    solr_array, es_array = [0.0]*6, [0.0]*6
     solr_time, solr_count_results, solr_results = [None, None, None]
     es_time, es_count_results, es_results = [None, None, None]
     fields = "keywords"
@@ -22,18 +23,39 @@ def index():
         search_word = request.form["searchWord"]
         field_keywords = request.form.get("keywords", False)
         field_abstract = request.form.get("abstract", False)
+        field_domain = request.form.get("domain", False)
 
         if field_keywords == "True":
             fields = "keywords"
         if field_abstract == "True":
             fields = "Abstract"
+        if field_domain == "True":
+            fields = "Domain"
 
-        solr_time, solr_count_results, solr_results = SolrSearch(fields, search_word)
-        es_time, es_count_results, es_results = ElasticSearch(fields, search_word)
+        # solr_time, solr_count_results, solr_results = SolrSearch(fields, search_word)
+        # es_time, es_count_results, es_results = ElasticSearch(fields, search_word)
+
+        for i in range(6):
+            solr_time, solr_count_results, solr_results = SolrSearch(fields, search_word)
+            es_time, es_count_results, es_results = ElasticSearch(fields, search_word)
+            solr_array[i] = solr_time
+            es_array[i] = es_time
+
+        print(solr_array)
+        print(es_array)
+        print("Solr Average:" + array_average(solr_array) + "\n" +
+              "ES Average:" + array_average(es_array))
 
     return render_template('index.html', my_title=my_title, numresults=solr_count_results, results=solr_results,
                            timeFin=solr_time, es_count_results=es_count_results, es_results=es_results,
                            es_finTime=es_time)
+
+
+def array_average(arr):
+    arr_avg = 0.0
+    for i in range(1, len(arr)):
+        arr_avg += arr[i]
+    return str(arr_avg / len(arr)-1)
 
 
 def SolrSearch(fields, search_word):
@@ -45,15 +67,8 @@ def SolrSearch(fields, search_word):
 
 
 def ElasticSearch(fields, search_word):
-    body = {
-        "query": {
-            "match": {
-                fields: search_word
-            }
-        }
-    }
     timerr.startTime()
-    res = elastic_url.search(body=body)
+    res = elastic_url.search(query={"match": {fields: search_word}})
     finish_time = timerr.finishTime()
     return finish_time, res['hits']['total']['value'], res['hits']['hits']
 
