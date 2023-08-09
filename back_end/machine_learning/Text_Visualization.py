c7088053e68cf230b1ad9a68785d9b9f81546671
Ayşenur DENİZ <anurdenizz@gmail.com>
2023-08-09 13:14:00 +0300

    update Text_Visualization.py

--- back_end/machine_learning/Text_Visualization.py
+++ back_end/machine_learning/Text_Visualization.py
@@ -43,3 +43,79 @@ def word_cloud(df, field, save=False):
     plt.show()
     if save == "True":
         wordcloud.to_file("wordcloud_white.png")
+
+
+# --------------------- heatmap for similarity scores -------------------------
+
+
+import matplotlib.pyplot as plt
+
+# Varsayılan font boyutunu ayarlayın
+plt.rcParams.update({'font.size': 17})  # Yeni font boyutunu burada belirleyin
+
+# Verileri düzenleyin (örnek veri)
+journals = [
+    "ADDITIVE MANUFACTURING",
+    "PROCEEDINGS OF THE INSTITUTION OF MECHANICAL ENGINEERS PART B-JOURNAL OF ENGINEERING MANUFACTURE",
+    "INTERNATIONAL JOURNAL OF ADVANCED MANUFACTURING TECHNOLOGY",
+    "3D PRINTING AND ADDITIVE MANUFACTURING",
+    "PROCESSES",
+    "JOURNAL OF MANUFACTURING PROCESSES",
+    "JOURNAL OF MANUFACTURING SCIENCE AND ENGINEERING-TRANSACTIONS OF THE ASME",
+    "CANADIAN GEOTECHNICAL JOURNAL",
+    "CHEMISTRY OF MATERIALS",
+    "SOLAR RRL",
+    "GEOTHERMAL ENERGY",
+    "CONTROL ENGINEERING PRACTICE",
+    "ACI MATERIALS JOURNAL",
+    "SURFACE COATINGS INTERNATIONAL",
+    "MATERIALS PERFORMANCE",
+    "ACI STRUCTURAL JOURNAL",
+    "J-FOR-JOURNAL OF SCIENCE & TECHNOLOGY FOR FOREST PRODUCTS AND PROCESSES"
+]
+techniques = ["TF-IDF", "BoW", "Word2Vec", "Doc2Vec", "N-grams"]
+similarity_scores = [
+    [0.707, 0.597, 0, 0, 0.688],
+    [0.411, 0, 0, 0, 0.631],
+    [0.378, 0, 0, 0, 0.601],
+    [0.373, 0.495, 0, 0, 0.605],
+    [0.364, 0.549, 0, 0, 0],
+    [0, 0.502, 0, 0, 0.64],
+    [0, 0.500, 0, 0, 0],
+    [0, 0, 0.997, 0, 0],
+    [0, 0, 0.997, 0, 0],
+    [0, 0, 0.993, 0, 0],
+    [0, 0, 0.993, 0, 0],
+    [0, 0, 0.993, 0, 0],
+    [0, 0, 0, 0.804, 0],
+    [0, 0, 0, 0.796, 0],
+    [0, 0, 0, 0.792, 0],
+    [0, 0, 0, 0.788, 0],
+    [0, 0, 0, 0.786, 0]
+]
+
+# Metin kırpma işlemi için maksimum karakter sayısı
+max_char_length = 30
+
+# Verileri ısı haritası olarak görselleştirin
+plt.figure(figsize=(15, 8))  # Boyutları artırarak metinlerin daha iyi görünmesini sağlayın
+plt.imshow(similarity_scores, cmap="Blues", aspect="auto", vmin=0, vmax=1)  # Sadece pozitif değerleri göster
+
+# Eksen etiketlerini ayarlayın (kırpılmış metinler)
+plt.xticks(range(len(techniques)), [tech[:max_char_length] for tech in techniques], rotation=45)
+plt.yticks(range(len(journals)), [journal[:max_char_length] for journal in journals])
+
+# Renk çubuğunu ekleyin
+plt.colorbar(label="Similarity Score")
+
+# Başlığı ve eksen etiketlerini ekleyin
+plt.title("Similarity Scores Heatmap for Journals and Techniques")
+plt.xlabel("Techniques")
+plt.ylabel("Journals")
+
+# Görseli gösterin
+plt.tight_layout()
+# Resmi kaydetmek için savefig kullanın
+plt.savefig("heatmap.png")  # Resim dosyasını istediğiniz isimle ve uzantıyla belirtin
+plt.show()
+
