d88ba39f82d71ed9d8022f963e8e0cfe0231f3f0
Ayşenur DENİZ <anurdenizz@gmail.com>
2023-08-14 13:54:20 +0300

    update Comparison_Similarity.py

--- back_end/machine_learning/Comparison_Similarity.py
+++ back_end/machine_learning/Comparison_Similarity.py
@@ -1,17 +1,21 @@
 import json
-import numpy as np
 import time
+
+import numpy as np
+import pandas as pd
+
+from back_end.machine_learning.FeatureEngineering import text_preprocessing
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from gensim.models import Word2Vec, Doc2Vec, KeyedVectors
+from gensim.models import Word2Vec, Doc2Vec
 from gensim.models.doc2vec import TaggedDocument
 from sklearn.metrics.pairwise import cosine_similarity
 
 
 class GenericMethods:
-    def __init__(self, data, user_input):
+    def __init__(self, data, user_input, texts):
         self.data = data
         self.user_input = user_input
-        self.texts = [entry["Aims and Scope"] for entry in self.data]
+        self.texts = texts
         self.cosine_similarities = None
         self.processing_time = None
 
@@ -19,42 +23,27 @@ class GenericMethods:
         pass
 
     def get_best_recommendations(self, top_k=5):
-        pass
+        indices = np.argsort(self.cosine_similarities[0])[-top_k:][::-1]
+        recommendations = [(self.data[index]['Journal Name'], self.cosine_similarities[0][index]) for index in indices]
+        return recommendations
 
 
 class TFIDFMethods(GenericMethods):
     def compute_similarity(self):
-        start_time = time.time()
         tfidf_vectorizer = TfidfVectorizer()
         tfidf_matrix = tfidf_vectorizer.fit_transform(self.texts + [self.user_input])
         self.cosine_similarities = cosine_similarity(tfidf_matrix[-1], tfidf_matrix[:-1])
-        end_time = time.time()
-        self.processing_time = end_time - start_time
-
-    def get_best_recommendations(self, top_k=5):
-        indices = np.argsort(self.cosine_similarities[0])[-top_k:][::-1]
-        recommendations = [(self.data[index]['Journal Name'], self.cosine_similarities[0][index]) for index in indices]
-        return recommendations
 
 
 class Word2VecMethods(GenericMethods):
     def compute_similarity(self):
-        start_time = time.time()
         word2vec_model = Word2Vec([text.split() for text in self.texts], vector_size=100, window=5, min_count=1)
         user_input_word2vec = np.mean([word2vec_model.wv[word] for word in self.user_input.split()], axis=0)
         self.cosine_similarities = cosine_similarity([user_input_word2vec], word2vec_model.wv.vectors)
-        end_time = time.time()
-        self.processing_time = end_time - start_time
-
-    def get_best_recommendations(self, top_k=5):
-        indices = np.argsort(self.cosine_similarities[0])[-top_k:][::-1]
-        recommendations = [(self.data[index]['Journal Name'], self.cosine_similarities[0][index]) for index in indices]
-        return recommendations
 
 
 class Doc2VecMethods(GenericMethods):
     def compute_similarity(self):
-        start_time = time.time()
         tagged_data = [TaggedDocument(words=text.split(), tags=[str(i)]) for i, text in enumerate(self.texts)]
         doc2vec_model = Doc2Vec(vector_size=100, window=2, min_count=1, workers=4, epochs=100)
         doc2vec_model.build_vocab(tagged_data)
@@ -62,60 +51,43 @@ class Doc2VecMethods(GenericMethods):
         user_input_doc2vec = doc2vec_model.infer_vector(self.user_input.split())
         self.cosine_similarities = cosine_similarity([user_input_doc2vec],
                                                      [doc2vec_model.dv[str(i)] for i in range(len(self.texts))])
-        end_time = time.time()
-        self.processing_time = end_time - start_time
-
-    def get_best_recommendations(self, top_k=5):
-        indices = np.argsort(self.cosine_similarities[0])[-top_k:][::-1]
-        recommendations = [(self.data[index]['Journal Name'], self.cosine_similarities[0][index]) for index in indices]
-        return recommendations
 
 
 class BowMethods(GenericMethods):
     def compute_similarity(self):
-        start_time = time.time()
         bow_vectorizer = CountVectorizer()
         bow_matrix = bow_vectorizer.fit_transform(self.texts + [self.user_input])
         self.cosine_similarities = cosine_similarity(bow_matrix[-1], bow_matrix[:-1])
-        end_time = time.time()
-        self.processing_time = end_time - start_time
-
-    def get_best_recommendations(self, top_k=5):
-        indices = np.argsort(self.cosine_similarities[0])[-top_k:][::-1]
-        recommendations = [(self.data[index]['Journal Name'], self.cosine_similarities[0][index]) for index in indices]
-        return recommendations
 
 
 class NgramMethods(GenericMethods):
     def compute_similarity(self):
-        start_time = time.time()
         ngram_vectorizer = CountVectorizer(analyzer='char', ngram_range=(2, 3))
         ngram_matrix = ngram_vectorizer.fit_transform(self.texts + [self.user_input])
         self.cosine_similarities = cosine_similarity(ngram_matrix[-1], ngram_matrix[:-1])
-        end_time = time.time()
-        self.processing_time = end_time - start_time
-
-    def get_best_recommendations(self, top_k=5):
-        indices = np.argsort(self.cosine_similarities[0])[-top_k:][::-1]
-        recommendations = [(self.data[index]['Journal Name'], self.cosine_similarities[0][index]) for index in indices]
-        return recommendations
 
 
 # JSON dosyasını okuyun
 with open('C:\\Users\\anurd\\Downloads\\wos.json', 'r') as file:
     data = json.load(file)
 
-user_input = "information systems retrieval knowledge discovery and data mining"
+df = pd.DataFrame(data)
+
+texts = text_preprocessing(df, "Aims and Scope")
+
+user_input = "information system retrieval knowledge discovery data mining"
 # user_input = "Novel additive manufacturing processes and techniques Improvements of established methods and materials"
 
 methods = [TFIDFMethods, BowMethods, Word2VecMethods, Doc2VecMethods, NgramMethods]
 
 for method_class in methods:
-    method = method_class(data, user_input)
+    started = time.time()
+    method = method_class(data, user_input, texts)
     method.compute_similarity()
     recommendations = method.get_best_recommendations(top_k=5)
+    distance = time.time() - started
 
     print("\nEn İyi Tavsiyeler ({})".format(method_class.__name__))
     for i, recommendation in enumerate(recommendations, start=1):
         print(f"{recommendation[0]},{recommendation[1]:.3f}")
-    print(f"Süre: {method.processing_time:.4f} s\n")
+    print(f"Süre: {distance:.4f} s\n")
